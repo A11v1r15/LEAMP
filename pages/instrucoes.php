d8ca7ea5327308d31274d3fe36742a5216c88d59
Álvaro Carvalho <a11v1r15@rocketmail.com>
2026-08-18 13:16:41 -0300

    Update instructions for system usage, login, and reviews

--- pages/instrucoes.php
+++ pages/instrucoes.php
@@ -7,15 +7,18 @@
 <h3>Leitura e busca</h3>
 
 <p>
-	&emsp;Use a página de livros para procurar obras disponíveis.
+	&emsp;Use a página de livros para procurar obras disponíveis. Obras "Pendente"
+	estão em processo de catalogação e serão disponibilizadas depois.
 </p>
 
 <h3>Login</h3>
 
 <p>
-	&emsp;O login é realizado usando
+	&emsp;O cadastro e login são realizados usando
 	contas Google "@gmail.com" e institucionais
 	do IFCE "@ifce.edu.br" ou "@aluno.ifce.edu.br".
+	Caso você seja aluno, é importante que se cadastre
+	com seu e-mail institucional.
 </p>
 <p>
 	&emsp;Se tiver problemas em acessar sua conta instituconal, entre em contato pelo e-mail
@@ -27,13 +30,18 @@
 
 <p>
 	&emsp;Os empréstimos são registrados
-	pela equipe responsável.
+	pela equipe responsável, retire os livros
+	na sala do ensino, ao lado da biblioteca.
+	Para fazer empréstimos, é necessário
+	estar cadastrado no sistema.
 </p>
 
-<h3>Comentários</h3>
+<h3>Resenhas</h3>
 
 <p>
-	&emsp;Comentários serão públicos quando aprovados.
+	&emsp;Para fazer resenhas das obras, acesse seu perfil e clique no botão "Escrever Resenha"
+	no card de emréstimo relacionado. As resenhas podem ser editadas posteriormente, caso necessário.
+	As resenhas serão públicas quando aprovadas e a aprovação é manual, por favor espere.
 	Mantenha respeito e bom senso.
 </p>
 
